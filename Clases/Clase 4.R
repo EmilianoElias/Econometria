7e2a26db8b99cbd8247085a006001ef0680bf2b0
Emiliano.Elias <emiliano.elias@alumnos.cide.edu>
2019-10-23 20:13:06 +0000

    Clase 4

--- Clases/Clase 4.R
+++ Clases/Clase 4.R
@@ -11,4 +11,3 @@ a <- anova(lm(ceosal1$lsalary ~ ceosal1$roe + ceosal1$lsales + ceosal1$ros))
 b <- anova(lm(ceosal1$lsalary ~ ceosal1$ros + ceosal1$lsales + ceosal1$roe))
 
 anova(lm(data$gdp_05 ~ data$capital + data$labor_force + data$secondary_educ + data$tfp + data$energy_productivity))
-
